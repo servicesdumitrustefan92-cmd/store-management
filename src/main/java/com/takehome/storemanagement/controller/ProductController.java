9c59cb96fbc09396c5c6911fdac072fdeccfae20
stefandumitru <services.dumitrustefan92@gmail.com>
2026-02-14 13:47:52 +0200

    fix: correct typo in ProductController

--- src/main/java/com/takehome/storemanagement/controller/ProductController.java
+++ src/main/java/com/takehome/storemanagement/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/prodducts")
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService service;
